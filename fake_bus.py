b8c55fcdc6ec3ec5b943020de0cfd2b830f3ef88
Igor Zakharov <igorzakhar@gmail.com>
2021-05-25 10:57:44 +0300

    Run the buses on all routes

--- fake_bus.py
+++ fake_bus.py
@@ -1,52 +1,66 @@
+import itertools
 import json
 import logging
+import os
 import sys
-import itertools
 
 import trio
+
+from trio_websocket import ConnectionClosed
 from trio_websocket import open_websocket_url
 
 
-async def load_bus_route(filename):
-    async with await trio.open_file(filename) as fp:
-        return await fp.read()
+def load_routes(directory_path='routes'):
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".json"):
+            filepath = os.path.join(directory_path, filename)
+            with open(filepath, 'r', encoding='utf8') as file:
+                yield json.load(file)
 
 
-async def send_coords():
-    bus_route = json.loads(await load_bus_route('bus_routes/156.json'))
-    coordinates = bus_route['coordinates']
+async def run_bus(url, bus_id, route):
+    coordinates = route['coordinates']
     route_cycle = itertools.cycle(coordinates)
 
     try:
-        async with open_websocket_url('ws://127.0.0.1:9000') as ws:
+        async with open_websocket_url(url) as ws:
             while True:
                 bus_coords = next(route_cycle)
                 lat, lng = bus_coords[0], bus_coords[1]
                 coordinates = {
-                    "busId": "1234567890",
-                    "lat": lat,
-                    "lng": lng,
-                    "route": bus_route["name"]
+                    'busId': f'{route["name"]}-0',
+                    'lat': lat,
+                    'lng': lng,
+                    'route': route['name']
                 }
 
-                await ws.send_message(json.dumps(coordinates))
+                message = json.dumps(coordinates, ensure_ascii=True)
+                await ws.send_message(message)
                 logging.debug(f'Sent message: {coordinates}')
 
                 await trio.sleep(1)
 
     except OSError as ose:
-        logging.exception(ose, exec_info=False)
+        logging.exception(ose, exc_info=False)
+    except ConnectionClosed:
+        exc_desc = f'Connection Closed {ws.local.address}:{ws.local.port}'
+        logging.exception(exc_desc, exc_info=False)
 
 
 async def main():
     logging.getLogger('trio-websocket').setLevel(logging.WARNING)
     logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 
-    await send_coords()
+    url = 'ws://127.0.0.1:9000'
+
+    async with trio.open_nursery() as nursery:
+        for route in load_routes():
+            bus_id = route['name']
+            nursery.start_soon(run_bus, url, bus_id, route)
 
 
 if __name__ == '__main__':
     try:
         trio.run(main)
-    except KeyboardInterrupt:
+    except (KeyboardInterrupt, trio.MultiError):
         sys.exit()
